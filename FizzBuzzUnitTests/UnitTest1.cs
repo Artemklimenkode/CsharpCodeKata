a9a96c55d1e25778b2de2f56612e345f9e62a403
Peter Ivanics <peter.ivanics@reslink.fi>
2016-03-21 23:08:08 +0200

    Added testIsDividableByFive()

--- FizzBuzzUnitTests/UnitTest1.cs
+++ FizzBuzzUnitTests/UnitTest1.cs
@@ -19,6 +19,21 @@ namespace FizzBuzzUnitTests
             Assert.AreEqual(true, BuzzThree.isDevidableByThree(6));
             Assert.AreEqual(true, BuzzThree.isDevidableByThree(0));
             Assert.AreEqual(false, BuzzThree.isDevidableByThree(-1));
-        }       
+        }
+
+        [TestMethod]
+        public void testIsDividableByFive() {
+            FizzBuzz testObject = new FizzBuzz();
+
+            Assert.IsTrue(testObject.isDevidableByFive(0));
+            Assert.IsTrue(testObject.isDevidableByFive(5));
+            Assert.IsTrue(testObject.isDevidableByFive(10));
+            Assert.IsTrue(testObject.isDevidableByFive(50));
+
+            Assert.IsFalse(testObject.isDevidableByFive(1));
+            Assert.IsFalse(testObject.isDevidableByFive(2));
+            Assert.IsFalse(testObject.isDevidableByFive(3));
+            Assert.IsFalse(testObject.isDevidableByFive(11));
+        }
     }
 }
