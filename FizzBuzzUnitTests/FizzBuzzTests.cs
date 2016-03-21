daa69a6af872b9b9559cba910a49368414e3230d
Peter Ivanics <peter.ivanics@reslink.fi>
2016-03-21 22:29:24 +0200

    Added unit test project

---
+++ FizzBuzzUnitTests/FizzBuzzTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzUnitTests
+{
+    /// <summary>
+    /// Summary description for FizzBuzzTests
+    /// </summary>
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
