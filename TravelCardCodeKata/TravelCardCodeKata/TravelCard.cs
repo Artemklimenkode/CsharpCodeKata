b82e910d23e897a6b4d6952d698b930ad26c0417
Artemklimenkode <Artemklimenko96@gmail.com>
2016-03-26 17:40:50 +0200

    created class TravelCard

---
+++ TravelCardCodeKata/TravelCardCodeKata/TravelCard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelCardCodeKata
+{
+    class TravelCard
+    {
+        bool buyTicket()
+        {
+            return false;
+        }
+        void buySeasonalTicket(int forDays)
+        {
+
+        }
+        void buySeasonalTicketForAmount(Double withBalance)
+        {
+
+        }
+        DateTime getSeasonalExpireDate()
+        {
+            DateTime expiryDate = new DateTime();
+            return expiryDate;
+        }
+        void extendBalance(double withAmount)
+        {
+
+        }
+        Double getBalance()
+        {
+            Double balance = new Double();
+            return balance;
+        }
+        void setDiscount(double discountValue)
+        {
+
+        }
+
+    }
+}
