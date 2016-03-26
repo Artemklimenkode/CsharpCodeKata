b82e812c5f0a4222373f97e230ac22f56e3e7f62
Peter Ivanics <peter.ivanics@reslink.fi>
2016-03-26 17:53:03 +0200

    Added TravelCardTests class with the first two tests implemented

---
+++ TravelCardCodeKata/TravelCardCodeKataUnitTests/TravelCardTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelCardCodeKata;
+
+namespace TravelCardCodeKataUnitTests
+{
+    [TestClass]
+    public class TravelCardTests
+    {
+        [TestMethod]
+        public void testBuyTicketWithMoneyOnTheCard() {
+            TravelCard testCard = new TravelCard();
+            testCard.extendBalance(50);
+
+            bool result = testCard.buyTicket();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void testBuyTicketWithoutMoneyOnTheCard() {
+            TravelCard testCard = new TravelCard();
+            bool result = testCard.buyTicket();
+
+            Assert.IsFalse(result);
+        }
+    }
+}
